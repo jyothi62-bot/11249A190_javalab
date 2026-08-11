159b8f2433b371b41e35f3732e764daf7b231912
11249a190 <11249A190@kanchiuniv.ac.in>
2026-08-11 13:05:52 +0530

    Add files via upload

---
+++ CharArray.java
@@ -0,0 +1,14 @@
+public class CharArray
+{
+    public static void main(String[] args)
+    {
+        char[] ch = {'J', 'A', 'V', 'A'};
+
+        System.out.println("Character Array:");
+
+        for(int i = 0; i < ch.length; i++)
+        {
+            System.out.print(ch[i] + " ");
+        }
+    }
+}
