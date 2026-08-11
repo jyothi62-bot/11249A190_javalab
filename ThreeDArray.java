590a872e33ea063c6b945d257abe47889a43d764
11249a190 <11249A190@kanchiuniv.ac.in>
2026-08-11 12:57:16 +0530

    Add files via upload

---
+++ ThreeDArray.java
@@ -0,0 +1,27 @@
+public class ThreeDArray
+{
+    public static void main(String[] args)
+    {
+        int[][][] arr =
+        {
+            {
+                {1, 2},
+                {3, 4}
+            }
+        };
+
+        System.out.println("Three-Dimensional Array:");
+
+        for(int i = 0; i < arr.length; i++)
+        {
+            for(int j = 0; j < arr[i].length; j++)
+            {
+                for(int k = 0; k < arr[i][j].length; k++)
+                {
+                    System.out.print(arr[i][j][k] + " ");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
